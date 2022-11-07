3dc3d5ace23e23a0eaea634fa5d182eed20fd004
datawhales <datawhales@gmail.com>
2022-11-07 16:14:56 +0900

    :sparkles: Add model download & model loading

--- src/model_registry_training/create_run.py
+++ src/model_registry_training/create_run.py
@@ -6,22 +6,28 @@ Description:
 """
 from mlflow import MlflowClient
 
-# Create a client of MLFlow Tracking Server
-client = MlflowClient(tracking_uri="http://127.0.0.1:5001")
 
-# Create MLFlow run
-test_experiment = client.get_experiment_by_name(name="Test Experiment")
+def create_run():
+    # Create a client of MLFlow Tracking Server
+    client = MlflowClient(tracking_uri="http://127.0.0.1:5000")
 
-run = client.create_run(
-    experiment_id=test_experiment.experiment_id,
-    tags={"Test Tag Key": "Test Tag Value"},
-    run_name="Test Run",
-)
+    # Create MLFlow run
+    test_experiment = client.get_experiment_by_name(name="Test Experiment")
 
-# Print created run metadata info to check if run is created correctly
-print(f"Run tags: {run.data.tags}")
-print(f"Experiment ID: {run.info.experiment_id}")
-print(f"Run ID: {run.info.run_id}")
-print(f"Run Name: {run.info.run_name}")
-print(f"Lifecycle_stage: {run.info.lifecycle_stage}")
-print(f"Status: {run.info.status}")
+    run = client.create_run(
+        experiment_id=test_experiment.experiment_id,
+        tags={"Test Tag Key": "Test Tag Value"},
+        run_name="Test Run",
+    )
+
+    # Print created run metadata info to check if run is created correctly
+    print(f"Run tags: {run.data.tags}")
+    print(f"Experiment ID: {run.info.experiment_id}")
+    print(f"Run ID: {run.info.run_id}")
+    print(f"Run Name: {run.info.run_name}")
+    print(f"Lifecycle_stage: {run.info.lifecycle_stage}")
+    print(f"Status: {run.info.status}")
+
+
+if __name__ == "__main__":
+    create_run()
---
+++ src/model_registry_training/load_model.py
@@ -0,0 +1,60 @@
+"""
+ML Model Load module
+
+Description:
+    This module loads the trained model from MLFlow Server.
+"""
+import os
+import mlflow
+from mlflow.client import MlflowClient
+import pandas as pd
+from sklearn.datasets import load_iris
+
+
+def download_model():
+    # Set MLFlow tracking uri
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+    # Get run info
+    df = mlflow.search_runs(experiment_names=["Test Experiment"])
+
+    # Get run_id
+    run_id = df[df["tags.mlflow.runName"] == "Iris"]["run_id"].item()
+    
+    client = MlflowClient()
+
+    os.makedirs("download", exist_ok=True)
+
+    download_path = client.download_artifacts(
+        run_id=run_id,
+        path="random-forest",
+        dst_path="download",
+    )
+
+    print(f"Artifacts downloaded in: {download_path}")
+    print(f"Artifacts: {os.listdir(download_path)}")
+
+
+def load_model():
+    model = mlflow.pyfunc.load_model(model_uri="download/random-forest")
+    return model
+
+
+def get_iris_sample():
+    # Load iris dataset
+    iris_dataset = load_iris()
+    features = iris_dataset["data"]
+    feature_names = ["sepal_length_cm", "sepal_width_cm", "petal_length_cm", "petal_width_cm"]
+
+    feature_df = pd.DataFrame(features, columns=feature_names)
+    return feature_df.sample(1)
+
+
+if __name__ == "__main__":
+    download_model()
+    model = load_model()
+    
+    iris_sample = get_iris_sample()
+    print(f"\nIris data sample:\n{iris_sample}")
+    pred = model.predict(iris_sample)
+    print(f"\nPrediction: {pred}")

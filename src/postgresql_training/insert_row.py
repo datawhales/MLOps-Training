43ada3ca3606e4adc915e64152fef059dee2180e
datawhales <datawhales@gmail.com>
2022-11-21 14:22:45 +0900

    :art: Modified table column names

--- src/postgresql_training/insert_row.py
+++ src/postgresql_training/insert_row.py
@@ -13,7 +13,7 @@ from postgresql_training.meta import MyDB
 # Load iris dataset
 iris_dataset = load_iris()
 features, labels = iris_dataset["data"], iris_dataset["target"]
-feature_names = ["sepal_length_cm", "sepal_width_cm", "petal_length_cm", "petal_width_cm"]
+feature_names = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
 label_names = ["target"]
 
 feature_df = pd.DataFrame(features, columns=feature_names)
@@ -35,7 +35,7 @@ with conn.cursor() as cursor:
     # Execute a SQL command (insert row)
     cursor.execute(
         """INSERT INTO iris_data (
-            sepal_length_cm, sepal_width_cm, petal_length_cm, petal_width_cm, target
+            sepal_length, sepal_width, petal_length, petal_width, target
         ) VALUES (%s, %s, %s, %s, %s)""", tuple(df.iloc[0].values)
     )
 
